5d0adf88dea7a36aa029a519a04fb2f29072acf8
Robi <ciobanurobert100@gmail.com>
2024-07-31 14:38:45 +0300

    added appointments table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { AppointmentComponent } from "./form/appointment/appointment.component";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { DateselectComponent } from './date/dateselect/dateselect.component';
+import {MatTableModule} from '@angular/material/table';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,11 +17,48 @@ import { DateselectComponent } from './date/dateselect/dateselect.component';
     MatExpansionModule,
     MatPaginatorModule,
     DateselectComponent,
+    MatTableModule,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  // date = Date.now();
+  HOURS_DATA = [
+    {hour: 0, appointment: []},
+    {hour: 1, appointment: []},
+    {hour: 2, appointment: []},
+    {hour: 3, appointment: []},
+    {hour: 4, appointment: []},
+    {hour: 5, appointment: []},
+    {hour: 6, appointment: []},
+    {hour: 7, appointment: []},
+    {hour: 8, appointment: []},
+    {hour: 9, appointment: []},
+    {hour: 10, appointment: []},
+    {hour: 11, appointment: []},
+    {hour: 12, appointment: []},
+    {hour: 13, appointment: []},
+    {hour: 14, appointment: []},
+    {hour: 15, appointment: []},
+    {hour: 16, appointment: []},
+    {hour: 17, appointment: []},
+    {hour: 18, appointment: []},
+    {hour: 19, appointment: []},
+    {hour: 20, appointment: []},
+    {hour: 21, appointment: []},
+    {hour: 22, appointment: []},
+    {hour: 23, appointment: []},
+    {hour: 24, appointment: []},
+  ];
 
+  displayedColumns: string[] = ['hour','appointment'];
+  dataSource = this.HOURS_DATA;
+
+  hourConvert(element:number) {
+    if(element <= 12) {
+      return `${ element }:00 AM`
+    } else {
+      return `${ element - 12 }:00 PM`
+    }
+  }
 }
